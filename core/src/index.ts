c748ccca4a7781540d082d1591a1bb451660fdaa
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2023-02-03 17:04:49 +0000

    wip

--- core/src/index.ts
+++ core/src/index.ts
@@ -1,4 +1,4 @@
-import { Readable, Subscriber, Unsubscriber, Writable, get, writable } from "./store"
+import { Readable, Subscriber, Unsubscriber, Writable, writable } from "./store"
 
 import { produce } from "immer"
 
@@ -9,46 +9,78 @@ export type Updater<S> = (s: S, ...parameters: any) => void
 export type Updaters<S> = {
     [key: string]: Updater<S>
 }
-export type Store<S, R extends Updaters<S>> = {
-    select: <T, TR extends Updaters<T>>(
-        selector: (s: S) => T, reducers: TR
+export type Reflector<S, T> = (s: S, value: T) => void
+
+export type Store<S, R extends Updaters<S> | undefined> = {
+    select: <T, RE extends (Reflector<S, T> | undefined), TR extends (Updaters<T> | undefined)>(
+        selector: (s: S) => T,
+        ...args: RE extends undefined ? [
+            reflect: Reflector<S, T>,
+            reducers: TR,
+        ] : []
     ) => Store<T, TR>,
+    subscribe: (subscriber: Subscriber<S>) => Unsubscriber,
+    update: (updater: (s: S) => S) => void
+} & (R extends Updaters<S> ? {
     actions: {
         [key in keyof R]: (
             ...parameters: WithoutFirst<R[key]>
         ) => ReturnType<R[key]>
     },
-    subscribe: (subscriber: Subscriber<S>) => Unsubscriber,
-}
+} : {})
 
-export function store<S, R extends Updaters<S>>(
-    initialState: S | Store<S, R>, reducers: R,
+export function store<S, R extends Updaters<S> | undefined>(
+    initialState: S, ...args: R extends Updaters<S> ? [reducers: R] : []
 ): Store<S, R> {
-    const state = writable<S>(null as S)
+    const [reducers] = args
 
-    if (
-        typeof initialState === "object" &&
-        initialState && "subscribe" in initialState
-    ) {
-        state.set(get(initialState))
-    }
-
+    const state = writable<S>(initialState)
     let currentState: S
     state.subscribe(value => currentState = value)
 
-    const actions: any = {}
-    for (const k in reducers) {
-        actions[k] = (...args: any) => {
+    const s: Store<S, R> = {
+        subscribe: state.subscribe,
+        select: (selector, ...args) => {
+            const [reflect, reducers] = args
+
+            const derived = store(
+                selector(currentState),
+                ...(reducers !== undefined ? [reducers] : []),
+            )
+            if (reflect !== undefined) {
+                derived.subscribe(value => {
+                    state.set(produce(
+                        currentState,
+                        // potentially confusing syntax, this returns s
+                        (s: S) => (reflect(s, value), s)
+                    ))
+                })
+            }
+            state.subscribe(s => derived.update(_ => selector(s)))
+            return derived
+        },
+        update: (updater: (s: S) => S) => {
             state.set(produce(
                 currentState,
-                (s: S) => reducers[k](s, ...args)
+                updater,
             ))
+        },
+        // ...(reducers !== undefined ? {
+        //     actions: {},
+        // } : {})
+    }
+
+    const actions: any = {}
+    if (reducers !== undefined) {
+        for (const k in reducers) {
+            actions[k] = (...args: any) => {
+                state.set(produce(
+                    currentState,
+                    (s: S) => reducers[k](s, ...args)
+                ))
+            }
         }
     }
 
-    return {
-        subscribe: state.subscribe,
-        select: (selector, reducers) => store(selector(currentState), reducers),
-        actions
-    }
+    return s
 }
